242dfb0a58a69fba2e7a5b2e38befc9960ace139
Adam Cattermole <a.d.cattermole@gmail.com>
2026-02-11 12:27:20 +0000

    Integration test for list data
    Signed-off-by: Adam Cattermole <a.d.cattermole@gmail.com>

--- tests/multi.rs
+++ tests/multi.rs
@@ -508,3 +508,189 @@ fn authenticated_one_ratelimit_action_matches() {
         .execute_and_expect(ReturnType::Action(Action::Continue))
         .unwrap();
 }
+
+#[test]
+#[serial]
+fn it_handles_array_metadata_in_predicates() {
+    let cfg = r#"{
+        "services": {
+            "authorino": {
+                "type": "auth",
+                "endpoint": "authorino-cluster",
+                "failureMode": "deny",
+                "timeout": "5s"
+            },
+            "limitador": {
+                "type": "ratelimit",
+                "endpoint": "limitador-cluster",
+                "failureMode": "deny",
+                "timeout": "5s"
+            }
+        },
+        "actionSets": [
+        {
+            "name": "test-groups",
+            "routeRuleConditions": {
+                "hostnames": ["*.toystore.com", "example.com"],
+                "predicates" : [
+                    "request.url_path.startsWith('/admin/toy')",
+                    "request.host == 'cars.toystore.com'",
+                    "request.method == 'POST'"
+                ]
+            },
+            "actions": [
+            {
+                "service": "authorino",
+                "scope": "authconfig-A"
+            },
+            {
+                "service": "limitador",
+                "scope": "RLS-domain",
+                "conditionalData": [
+                {
+                    "predicates": [
+                        "auth.identity.groups.exists(g, g == 'basic')"
+                    ],
+                    "data": [
+                    {
+                        "static": {
+                            "key": "limit.basic_limit",
+                            "value": "10"
+                        }
+                    }]
+                }]
+            }]
+        }]
+    }"#;
+
+    let args = tester::MockSettings {
+        wasm_path: wasm_module(),
+        quiet: false,
+        allow_unexpected: false,
+    };
+    let mut module = tester::mock(args).unwrap();
+
+    module
+        .call_start()
+        .execute_and_expect(ReturnType::None)
+        .unwrap();
+
+    let root_context = 1;
+
+    module
+        .call_proxy_on_context_create(root_context, 0)
+        .expect_log(Some(LogLevel::Info), Some("#1 set_root_context"))
+        .execute_and_expect(ReturnType::None)
+        .unwrap();
+
+    module
+        .call_proxy_on_configure(root_context, 0)
+        .expect_log(Some(LogLevel::Info), Some("#1 on_configure"))
+        .expect_define_metric(Some(MetricType::Counter), Some("kuadrant.configs"))
+        .returning(Some(1))
+        .expect_define_metric(Some(MetricType::Counter), Some("kuadrant.hits"))
+        .returning(Some(2))
+        .expect_define_metric(Some(MetricType::Counter), Some("kuadrant.misses"))
+        .returning(Some(3))
+        .expect_define_metric(Some(MetricType::Counter), Some("kuadrant.allowed"))
+        .returning(Some(4))
+        .expect_define_metric(Some(MetricType::Counter), Some("kuadrant.denied"))
+        .returning(Some(5))
+        .expect_define_metric(Some(MetricType::Counter), Some("kuadrant.errors"))
+        .returning(Some(6))
+        .expect_increment_metric(Some(1), Some(1))
+        .expect_get_buffer_bytes(Some(BufferType::PluginConfiguration))
+        .returning(Some(cfg.as_bytes()))
+        .expect_get_log_level()
+        .returning(Some(LOG_LEVEL))
+        .execute_and_expect(ReturnType::Bool(true))
+        .unwrap();
+
+    let http_context = 2;
+    module
+        .call_proxy_on_context_create(http_context, root_context)
+        .expect_get_log_level()
+        .returning(Some(LOG_LEVEL))
+        .execute_and_expect(ReturnType::None)
+        .unwrap();
+
+    module
+        .call_proxy_on_request_headers(http_context, 0, false)
+        .expect_get_property(Some(vec!["request", "host"]))
+        .returning(Some(data::request::HOST))
+        // retrieving properties for conditions
+        .expect_get_property(Some(vec!["request", "url_path"]))
+        .returning(Some(data::request::path::ADMIN_TOY))
+        .expect_get_property(Some(vec!["request", "method"]))
+        .returning(Some(data::request::method::POST))
+        // retrieving properties for CheckRequest
+        .expect_get_header_map_pairs(Some(MapType::HttpRequestHeaders))
+        .returning(None)
+        .expect_get_property(Some(vec!["request", "scheme"]))
+        .returning(Some(data::request::scheme::HTTP))
+        .expect_get_property(Some(vec!["request", "path"]))
+        .returning(Some(data::request::path::ADMIN_TOY))
+        .expect_get_property(Some(vec!["request", "protocol"]))
+        .returning(Some(data::request::protocol::HTTP_1_1))
+        .expect_get_property(Some(vec!["request", "time"]))
+        .returning(Some(data::request::TIME))
+        .expect_get_property(Some(vec!["destination", "address"]))
+        .returning(Some(data::destination::ADDRESS))
+        .expect_get_property(Some(vec!["destination", "port"]))
+        .returning(Some(data::destination::port::P_8000))
+        .expect_get_property(Some(vec!["source", "address"]))
+        .returning(Some(data::source::ADDRESS))
+        .expect_get_property(Some(vec!["source", "port"]))
+        .returning(Some(data::source::port::P_45000))
+        .expect_increment_metric(Some(2), Some(1))
+        .expect_grpc_call(
+            Some("authorino-cluster"),
+            Some("envoy.service.auth.v3.Authorization"),
+            Some("Check"),
+            None,
+            None,
+            Some(5000),
+        )
+        .returning(Ok(42))
+        .execute_and_expect(ReturnType::Action(Action::Pause))
+        .unwrap();
+
+    let grpc_response = data::auth_response::WITH_METADATA_GROUPS;
+    module
+        .call_proxy_on_grpc_receive(http_context, 42, grpc_response.len() as i32)
+        .expect_get_buffer_bytes(Some(BufferType::GrpcReceiveBuffer))
+        .returning(Some(grpc_response))
+        .expect_set_property(
+            Some(vec!["kuadrant.auth.identity.groups"]),
+            Some(b"[\"basic\",\"premium\"]"),
+        )
+        .expect_set_property(
+            Some(vec!["kuadrant.auth.identity.userid"]),
+            Some(b"\"alice\""),
+        )
+        .expect_grpc_call(
+            Some("limitador-cluster"),
+            Some("envoy.service.ratelimit.v3.RateLimitService"),
+            Some("ShouldRateLimit"),
+            None,
+            None,
+            Some(5000),
+        )
+        .returning(Ok(43))
+        .execute_and_expect(ReturnType::None)
+        .unwrap();
+
+    let grpc_response: [u8; 2] = [8, 1];
+    module
+        .call_proxy_on_grpc_receive(http_context, 43, grpc_response.len() as i32)
+        .expect_get_buffer_bytes(Some(BufferType::GrpcReceiveBuffer))
+        .returning(Some(&grpc_response))
+        .execute_and_expect(ReturnType::None)
+        .unwrap();
+
+    module
+        .call_proxy_on_response_headers(http_context, 0, false)
+        .expect_increment_metric(Some(4), Some(1))
+        .execute_and_expect(ReturnType::Action(Action::Continue))
+        .unwrap();
+}
